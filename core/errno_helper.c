e55822e8f5f09faf44cf2bb8e873dce66ef80dff
grimtin10 <grimtin10@gmail.com>
2025-02-16 21:19:10 -0700

    begin rainbow v1 rewrite

--- core/errno_helper.c
+++
@@ -1,5 +0,0 @@
-#include <errno.h>
-
-int get_errno() {
-    return errno;
-}
--- src/block.rs
+++
@@ -1,7 +0,0 @@
-use crate::{instruction::Instruction, scope::Scope};
-
-#[derive(Debug, Clone)]
-pub enum Block {
-    CODE(Vec<Instruction>),
-    SCOPE(Scope),
-}
